7b8c9f62d2edb2e2f58a59757b178652c08bfea0
Srinivas Chennareddy <104940622+chsfinancialgroup@users.noreply.github.com>
2022-05-04 17:17:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado Toast')
 
-streamlit.header('Bild your own Fruit Smoothie')
+streamlit.header('Build your own Fruit Smoothie')
